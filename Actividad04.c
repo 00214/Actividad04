517f6594a5525a157013d7fb77f3283d42e6a1e1
Raúl Baeza <raul.baeza3078@alumnos.udg.mx>
2020-09-17 16:19:55 -0500

    Implementación de la función capturar_enteros y otras correciones

--- Actividad04.c
+++ Actividad04.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 
-int main()
+void capturar_enteros()
+{
+    int enteros[5];
+    int promedio = 0;
+    int suma = 0;
+
+    printf("Ingresa 5 numeros enteros\n");
+
+    for (size_t i = 0; i < 5; i++) // Captura los 5 enteros 
+    {
+        scanf("%i",&enteros[i]);
+    }
+    for (size_t i = 0; i < 5; i++) // Imprime los 5 enteros
+    {
+        printf(" [%i] \n",enteros[i]);
+    }
+    for (size_t i = 0; i < 5; i++) // Suma de enteros en la posición i
+    {
+        suma = suma + enteros[i]; 
+    }
+    printf("Suma: %i \n",suma); // Imprime la suma de los enteros
+    printf("Promedio: %.2f \n",suma/5.0); // Imprime el promedio de los enteros
+}
+
+int main() //Función Principal
 {
     char opcion;
     char cadena[50];
@@ -8,10 +32,8 @@ int main()
 
     do
     {
-
-
-        printf("Actividad 04 / Registro de persoanjes\n\n");
-        printf("1) Captura de enteros\n");
+        printf("Actividad 04 / Registro de personajes\n\n");
+        printf("1) Captura de numeros enteros\n");
         printf("2) Captura y muestra n veces la cadena\n");
         printf("3) Agregar personajes\n");
         printf("4) Mostrar Personajes\n");
@@ -21,7 +43,7 @@ int main()
         switch (opcion)
         {
         case '1':
-            /* code */
+            capturar_enteros();
             break;
         case '2':
             /* code */
@@ -33,12 +55,13 @@ int main()
             /* code */
             break;
         
-        default: printf("No existe esta opcion");
+        default:
             break;
         }
 
         fflush(stdin);
     }while(opcion !='0');
+    printf("Hasta pronto!\n");
     
     return 0;
 }
